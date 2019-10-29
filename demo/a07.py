d2c7412d57c7a0ca0c933b861969cb8bd84335da
oxxo <oxxo.studio@gmail.com>
2019-10-30 00:34:19 +0800

    add a07.py

---
+++ demo/a07.py
@@ -0,0 +1,14 @@
+#encoding:UTF-8
+import random
+
+arr = []
+for i in range(1, 50):
+  arr.append(i)
+
+lto = []
+for i in range(6):
+  arrLength = int(len(arr)) - 1
+  lto.append(arr.pop(random.randint(0, arrLength)))
+
+lto.sort()
+print(lto)
--- readme.md
+++ readme.md
@@ -4,5 +4,6 @@
 - [猜數字 ( 幾 A 幾 B )](demo/a02.py)
 - [輸入單一文字，取得該文字的 ASCII Code](demo/a03.py)
 - [九九乘法表](demo/a04.py)
+- [大樂透 49 取 6](demo/a07.py)
 - [requests GET 測試](demo/a05.py)
 - [requests GET 讀取資料後寫入 txt](demo/a06.py)
