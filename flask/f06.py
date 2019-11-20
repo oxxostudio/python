bfa495ee50eda97bc72183319438ba2048e6c00c
oxxo <oxxo.studio@gmail.com>
2019-11-21 00:20:51 +0800

    update demo

--- flask/f06.py
+++ flask/f06.py
@@ -1,12 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 可接受跨域
 
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def hello():
     a = request.args
-    return f'{a["aa"]},{a["bb"]}'
+    return a
 
 
+#app.run(port=3000, host='0.0.0.0', debug=True) # host 可以改 ip
 app.run(port=3000, debug=True)
